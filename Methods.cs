f0440c81225fff236e2294cfebe2c24a7b16035c
AmirHossein Zare <130905177+Amirhossein-zare@users.noreply.github.com>
2024-03-28 01:40:24 +0330

    Methods Class
    creating methods to use them in program

---
+++ Methods.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Panel_Uni
+{
+    internal class Methods
+    {
+        public static void PrintJustNumber(string word)
+        {
+            DateTime.Now.PrintDateTime();
+            Console.WriteLine("Warning !!!\n");
+            Thread.Sleep(2000);
+            Console.WriteLine("\tJust Numbers");
+            Console.WriteLine("\tYou can type only number for {0}",word);
+            Console.WriteLine("\tPlease wait a moment");
+            Thread.Sleep(5000);
+            DateTime.Now.PrintDateTime();
+        }
+        public static void PrintJustEnglish() 
+        {
+            DateTime.Now.PrintDateTime();
+            Console.WriteLine("Warning !!!\n");
+            Thread.Sleep(2000);
+            Console.WriteLine("\tWe understood");
+            Console.WriteLine("\tBut remember to type only in English");
+            Console.WriteLine("\tplease wait a moment");
+            Thread.Sleep(5000);
+        }
+    }
+}
